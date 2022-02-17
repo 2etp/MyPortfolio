58eff7f2e05fd9d899c7fe9cc506d2ded3ea47e4
2etp <2etp@daum.net>
2022-02-17 18:19:01 +0900

    2022.02.17 학지시 수정

--- 학지시_메인.js
+++ 학지시_메인.js
@@ -71,4 +71,55 @@ changeBtn2.addEventListener("click", mainTabClick2);
 changeBtn3.addEventListener("click", mainTabClick3); 
 changeBtn4.addEventListener("click", mainTabClick4); 
 changeBtn5.addEventListener("click", mainTabClick5); 
-changeBtn6.addEventListener("click", mainTabClick6); 
+changeBtn6.addEventListener("click", mainTabClick6);
+
+let slideIndex = 0; // slide index
+
+// HTML 로드가 끝난 후 동작
+window.onload = function(){
+    showSlides(slideIndex);
+
+    // Auto Move Slide
+  let sec = 3000;
+  setInterval(function(){
+    slideIndex++;
+    showSlides(slideIndex);
+
+  }, sec);
+}
+
+// Next/previous controls
+function moveSlides(n) {
+    slideIndex = slideIndex + n
+    showSlides(slideIndex);
+  }
+  
+  // Thumbnail image controls
+function currentSlide(n) {
+    slideIndex = n;
+    showSlides(slideIndex);
+  }
+  
+function showSlides(n) {
+  
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    let size = slides.length;
+  
+    if ((n+1) > size) {
+      slideIndex = 0; n = 0;
+    }else if (n < 0) {
+      slideIndex = (size-1);
+      n = (size-1);
+    }
+  
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+  
+    slides[n].style.display = "block";
+    dots[n].className += " active";
+  }
