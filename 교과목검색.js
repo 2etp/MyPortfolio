c9eb9324d465dcca608fd9c1592d2af312b56ec6
2etp <2etp@daum.net>
2022-02-27 22:12:50 +0900

    1. 테이블 스타일 조정 2. 창 크기에 상관없이 요소들을 항상 가운데 정렬하기 3. 검색 버튼 추가

--- 교과목검색.js
+++ 교과목검색.js
@@ -1,4 +1,4 @@
 // 버튼 클릭 시 팝업 기능
 function popUp() {
-    window.open("교과목검색.html", "subjectSearch", "width=800px, height=900px, left=200px, top=100px");
+    window.open("교과목검색.html", "subjectSearch", "width=1000px, height=900px, left=200px, top=100px");
 }
