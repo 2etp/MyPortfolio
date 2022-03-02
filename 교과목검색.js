105f1818f53e7f54ff7c07cbe6cd0fbfe7b94421
2etp <2etp@daum.net>
2022-03-02 18:00:06 +0900

    교과목검색 테이블의 정보를 희과담 테이블에 집어넣는 시도를 하려고 했으나 실패함

--- 교과목검색.js
+++ 교과목검색.js
@@ -2,3 +2,10 @@
 function popUp() {
     window.open("교과목검색.html", "subjectSearch", "width=1000px, height=900px, left=200px, top=100px");
 }
+
+const courseCode = document.getElementById("course_code");
+
+courseCode.addEventListener('click', () => {
+
+    
+});
