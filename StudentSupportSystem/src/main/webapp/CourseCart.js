65e42d04f4c9acd66b419501587ce8190bd18f9c
2etp <2etp@daum.net>
2022-05-09 22:45:51 +0900

    토글 시도 중

--- StudentSupportSystem/src/main/webapp/CourseCart.js
+++ StudentSupportSystem/src/main/webapp/CourseCart.js
@@ -1,18 +1,26 @@
 var applyChk = document.querySelectorAll(".applyChk");
 console.log("applyChk.length : " + applyChk.length);
 
+var apply;
+console.log("apply(1) : " + apply);
 	for (let i = 0; i < applyChk.length; ++i) {
-		
-		let num = i;
-	
-		var apply = document.getElementById(num);
+
+		apply = document.getElementById(i);
 		console.log(apply);
 		console.log(apply.id);
 		}
 function buttonToggle() {
-		if(apply.id == num && apply.value=="신청") {
+		if(apply.id == "0" && apply.value=="신청") {
 			apply.value="신청완료";
-		}else if(apply.id == num && apply.value=="신청완료") {
+			console.log("apply.id == 0: " + apply.id == "0");
+		}else if(apply.id == "0" && apply.value=="신청완료") {
+			apply.value="신청";
+		}
+		
+		if(apply.id == "1" && apply.value=="신청") {
+			apply.value="신청완료";
+			console.log("apply.id == 1: " + apply.id == "1");
+		}else if(apply.id == "1" && apply.value=="신청완료") {
 			apply.value="신청";
 		}
 		
